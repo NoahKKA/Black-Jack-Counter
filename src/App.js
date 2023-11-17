0ae32e107a8b40af4f3d9e38224de019b69dfc5b
NoahKKA <noahhhhh.a@gmail.com>
2023-11-17 04:07:40 -0800

    setting up sub directories such as componets and pages.  Also worked on readMe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePage } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <h1>Intro</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import NavBar from "../componets/NavBar.jsx"
+
+export default function HomePage(){
+    return (
+        <div>
+            <NavBar/>
+            Hello
+        </div>
+    )
+}
